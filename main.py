bb475261f0f77b7c9cd8e25fda80a49361031395
Komissarich <johndoe@example.com>
2022-12-27 17:35:15 +0400

    a

--- main.py
+++ main.py
@@ -2,61 +2,62 @@ import os
 import sys
 
 import pygame
-
+import random
 pygame.init()
-size = width, height = 600, 95
+size = width, height = 600, 600
 screen = pygame.display.set_mode(size)
 pygame.display.flip()
 screen.fill((255, 255, 255))
 
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
-
+horizontal_borders = pygame.sprite.Group()
+vertical_borders = pygame.sprite.Group()
 
 all_sprites = pygame.sprite.Group()
 
 
-class Car(pygame.sprite.Sprite):
-    def __init__(self, name):
-        super().__init__()
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, radius, x, y):
+        super().__init__(all_sprites)
+        self.radius = radius
+        self.image = pygame.Surface((2 * radius, 2 * radius),
+                                    pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("red"),
+                           (radius, radius), radius)
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = random.randint(-5, 5)
+        self.vy = random.randrange(-5, 5)
 
-        self.image = load_image(name)
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
+        if pygame.sprite.spritecollideany(self, horizontal_borders):
+            self.vy = -self.vy
+        if pygame.sprite.spritecollideany(self, vertical_borders):
+            self.vx = -self.vx
 
-        self.rect = self.image.get_rect()
-        self.car_right = self.image
-        self.car_left = pygame.transform.flip(self.car_right, True, False)
+class Border(pygame.sprite.Sprite):
+    # строго вертикальный или строго горизонтальный отрезок
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:  # вертикальная стенка
+            self.add(vertical_borders)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:  # горизонтальная стенка
+            self.add(horizontal_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
 
-    def moving(self, direction1):
-
-        if direction1:
-            self.image = self.car_right
-            self.rect.x += 1
-        else:
-            self.image = self.car_left
-            self.rect.x -= 1
 
 
-a = "img.png"
+Border(5, 5, width - 5, 5)
+Border(5, height - 5, width - 5, height - 5)
+Border(5, 5, 5, height - 5)
+Border(width - 5, 5, width - 5, height - 5)
+balls = []
+for i in range(10):
+    a = Ball(20, 300, 300)
+    balls.append(a)
 
-car = Car(a)
-all_sprites.add(car)
-
-direction = True
 
 pygame.display.flip()
 fps = 50
@@ -68,12 +69,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    if car.rect.x > 455:
-        direction = False
-    if car.rect.x < 10:
-        direction = True
-
-    car.moving(direction)
+    for i in balls:
+        i.update()
 
     pygame.display.update()
     screen.fill((255, 255, 255))
