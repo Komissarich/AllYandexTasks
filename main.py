e70ee20230377432e8e49245618321717ea9c259
Komissarich <115020706+Komissarich@users.noreply.github.com>
2023-03-08 13:04:59 +0400

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ def index(letter):
     return render_template('base.html', title= letter)
 
 
+@app.route('/training/<specialization>')
+def prof(specialization):
+    if 'инженер' in specialization:
+        return render_template('prof.html', title=specialization, prof='Инженерный симулятор', path="/static/img/eng.jpg")
+    elif 'строитель' in specialization:
+        return render_template('prof.html', title=specialization, prof="Строительный симулятор", path="/static/img/builder.jpg")
+    else:
+        return render_template('prof.html', title=specialization, prof="Научный симулятор", path="/static/img/science.jpg")
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
