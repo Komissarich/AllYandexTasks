e34dfd1c8ace66345078100b4de18e79e0c4f13a
Komissarich <johndoe@example.com>
2023-03-12 14:33:55 +0400

    a

--- main.py
+++ main.py
@@ -1,81 +1,40 @@
-import os
-import sys
+from flask import Flask, url_for, request, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
-import pygame
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-pygame.init()
-size = width, height = 600, 95
-screen = pygame.display.set_mode(size)
-pygame.display.flip()
-screen.fill((255, 255, 255))
+class LoginForm(FlaskForm):
+    username = StringField('id астронавта', validators=[DataRequired()])
+    password = StringField('Пароль астронавта', validators=[DataRequired()])
+    captain_name = StringField('id капитана', validators=[DataRequired()])
+    password_captain = StringField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
 
-
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
+@app.route('/')
+def a():
+    return "Миссия Колонизация Марса"
 
 
-all_sprites = pygame.sprite.Group()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('autonom.html', title='Авторизация', form=form)
 
 
-class Car(pygame.sprite.Sprite):
-    def __init__(self, name):
-        super().__init__()
-
-        self.image = load_image(name)
-
-        self.rect = self.image.get_rect()
-        self.car_right = self.image
-        self.car_left = pygame.transform.flip(self.car_right, True, False)
-
-    def moving(self, direction1):
-
-        if direction1:
-            self.image = self.car_right
-            self.rect.x += 1
-        else:
-            self.image = self.car_left
-            self.rect.x -= 1
+@app.route('/b')
+def b():
+    return "fsaedaefawdxcfsfvcbv"
 
 
-a = "car2.png"
+@app.route('/success')
+def succes():
+    return "Успешно"
 
-car = Car(a)
-all_sprites.add(car)
 
-direction = True
-
-pygame.display.flip()
-fps = 50
-v = 60
-clock = pygame.time.Clock()
-running = True
-while running:
-    all_sprites.draw(screen)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    if car.rect.x > 455:
-        direction = False
-    if car.rect.x < 10:
-        direction = True
-
-    car.moving(direction)
-
-    pygame.display.update()
-    screen.fill((255, 255, 255))
-
-    clock.tick(fps)
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
