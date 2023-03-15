baf29dbb7310bb5bdc2a923606fdfa8c05b13cbd
Komissarich <115020706+Komissarich@users.noreply.github.com>
2023-03-15 18:50:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,67 @@
-import os
-import sys
-
-import pygame
-
-pygame.init()
-size = width, height = 600, 95
-screen = pygame.display.set_mode(size)
-pygame.display.flip()
-screen.fill((255, 255, 255))
+from flask import Flask
+from data import db_session
+from data.users import User
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-def load_image(name, colorkey=None):
-    fullname = os.path.join('data', name)
-
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-
-    image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
+def main():
+    db_session.global_init("db/blogs.db")
+    cap = User()
+    cap.surname = "Scott"
+    cap.name = "Ridley"
+    cap.age = 21
+    cap.position = "captain"
+    cap.speciality = "research engineer"
+    cap.address = "module_1"
+    cap.email = "scott_chief@mars.org"
+    cap.hashed_password = "cap"
+    db_sess = db_session.create_session()
+    db_sess.add(cap)
+    db_sess.commit()
 
 
-all_sprites = pygame.sprite.Group()
+    cap = User()
+    cap.surname = "Andrew"
+    cap.name = "Bolkonskii"
+    cap.age = 21
+    cap.position = "engineer"
+    cap.speciality = "idk"
+    cap.address = "module_2"
+    cap.email = "andrew_chief@earth.org"
+    cap.hashed_password = "cop"
+    db_sess = db_session.create_session()
+    db_sess.add(cap)
+    db_sess.commit()
 
 
-class Car(pygame.sprite.Sprite):
-    def __init__(self, name):
-        super().__init__()
-
-        self.image = load_image(name)
-
-        self.rect = self.image.get_rect()
-        self.car_right = self.image
-        self.car_left = pygame.transform.flip(self.car_right, True, False)
-
-    def moving(self, direction1):
-
-        if direction1:
-            self.image = self.car_right
-            self.rect.x += 1
-        else:
-            self.image = self.car_left
-            self.rect.x -= 1
+    cap = User()
+    cap.surname = "Donald"
+    cap.name = "Trump"
+    cap.age = 21
+    cap.position = "president"
+    cap.speciality = "lol"
+    cap.address = "module_5"
+    cap.email = "USA@mars.org"
+    cap.hashed_password = "pr"
+    db_sess = db_session.create_session()
+    db_sess.add(cap)
+    db_sess.commit()
 
 
-a = "car2.png"
+    cap = User()
+    cap.surname = "Vladimir"
+    cap.name = "Lenin"
+    cap.age = 21
+    cap.position = "Vojd"
+    cap.speciality = "communist"
+    cap.address = "module_7"
+    cap.email = "USSR@mars.org"
+    cap.hashed_password = "vo"
+    db_sess = db_session.create_session()
+    db_sess.add(cap)
+    db_sess.commit()
 
-car = Car(a)
-all_sprites.add(car)
+if __name__ == '__main__':
+    main()
 
-direction = True
-
-pygame.display.flip()
-fps = 50
-v = 60
-clock = pygame.time.Clock()
-running = True
-while running:
-    all_sprites.draw(screen)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    if car.rect.x > 455:
-        direction = False
-    if car.rect.x < 10:
-        direction = True
-
-    car.moving(direction)
-
-    pygame.display.update()
-    screen.fill((255, 255, 255))
-
-    clock.tick(fps)
