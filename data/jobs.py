4dfc38a38160613bee58fdb9a8be819c5a946057
Komissarich <115020706+Komissarich@users.noreply.github.com>
2023-03-18 18:25:09 +0400

    Update jobs.py

--- data/jobs.py
+++ data/jobs.py
@@ -1,20 +1,15 @@
 import datetime
 import sqlalchemy
 from sqlalchemy import orm
-
+import sqlalchemy_serializer
 from .db_session import SqlAlchemyBase
-
-class Jobs(SqlAlchemyBase):
+import flask_login
+class Jobs(SqlAlchemyBase,flask_login.UserMixin, sqlalchemy_serializer.SerializerMixin):
     __tablename__ = 'jobs'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    team_leader = sqlalchemy.Column(sqlalchemy.String, sqlalchemy.ForeignKey("users.id"))
+    name = sqlalchemy.Column(sqlalchemy.String, sqlalchemy.ForeignKey("users.id"))
     work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
     collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    start_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                   default=datetime.datetime.now)
-    end_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                 default=datetime.datetime.now)
-    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)
-    user = orm.relationship('User')
+
