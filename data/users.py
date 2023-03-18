52ff16af0dc2643f74e10f951f172a87cc2f7b0b
Komissarich <115020706+Komissarich@users.noreply.github.com>
2023-03-18 18:52:31 +0400

    Update users.py

--- data/users.py
+++ data/users.py
@@ -15,6 +15,4 @@ class User(SqlAlchemyBase):
     speciality = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String, nullable=True, unique=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                      default=datetime.datetime.now)
+
